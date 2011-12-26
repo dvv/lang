62be57b8958885b3f63f3b163a4b0aed8188ec8e
Vladimir Dronnikov <dronnikov@gmail.com>
2011-12-26 10:24:44 -0500

    fixate

--- tests/smoke.js
+++ tests/smoke.js
@@ -46,30 +46,25 @@ require('vows').describe('smoke')
     'has sane defaults': function (L) {
       ok(L);
       ok(L.hash);
-      equal(L.options.tags.open, '#{');
-      equal(L.options.tags.close, '}');
+      ok(!L.options.tags);
       ok(!L.compiler);
     },
   },
-  'locale adds no significant overhead': {
+  'locale with optinal tags:': {
     topic: function () {
-      var localeIds = [
-        'en', 'ja', 'vi', 'ko', 'pt-BR', 'fr', 'lv', 'br', 'ga', 'gd', 'cy',
-        'ro', 'lt', 'ru', 'uk', 'sr', 'hr', 'sh', 'cs', 'sk', 'pl', 'mk', 'sl'
-      ];
-      var plurals = require('../lib/plurals');
-      var lengths = {};
-      for (var i = 0; i < localeIds.length; i++) {
-        var L = new Locale();
-        L.add(localeIds[i], locales.en);
-        var s = L.get('user.family1');
-        lengths[localeIds[i]] = Math.round(s.length / locales.en.user.family1.length * 100) / 100;
-      }
-      return lengths;
+      return new Locale({
+        tags: {
+          open: '#{',
+          close: '}'
+        }
+      });
     },
-    'escaping': function(lengths) {
-      ok(lengths);
-      console.log(lengths);
+    'has tags': function (L) {
+      ok(L);
+      ok(L.hash);
+      equal(L.options.tags.open, '#{');
+      equal(L.options.tags.close, '}');
+      ok(!L.compiler);
     },
   },
   'locale accepts additions': {
@@ -183,6 +178,7 @@ require('vows').describe('smoke')
       var L = new Locale({
       });
       L.add('en', {
+        bar:'У тебя #{wives} #{wives   жена|жены|жён} и #{children} %{ребёнок}}|ребёнка}}}}|детей}}}}}}}:children',
         foo: '#{a a}}|b}}|cc}}}}}'
       });
       return L;
