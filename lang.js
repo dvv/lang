f684844885e94881f516e80295d5254e66f77acd
Vladimir Dronnikov <dronnikov@gmail.com>
2011-12-27 04:51:03 +0000

    recompiled client

--- lang.js
+++ lang.js
@@ -1,4 +1,292 @@
-(function () { function require(p){ var path = require.resolve(p) , mod = require.modules[path]; if (!mod) throw new Error('failed to require "' + p + '"'); if (!mod.exports) { mod.exports = {}; mod.call(mod.exports, mod, mod.exports, require.relative(path)); } return mod.exports;}require.modules = {};require.resolve = function(path){ var orig = path , reg = path + '.js' , index = path + '/index.js'; return require.modules[reg] && reg || require.modules[index] && index || orig;};require.register = function(path, fn){ require.modules[path] = fn;};require.relative = function(parent) { return function(p){ if ('.' != p.charAt(0)) return require(p); var path = parent.split('/') , segs = p.split('/'); path.pop(); for (var i = 0; i < segs.length; i++) { var seg = segs[i]; if ('..' == seg) path.pop(); else if ('.' != seg) path.push(seg); } return require(path.join('/')); };};require.register("compose.js", function(module, exports, require){
+(function () { function require(p){ var path = require.resolve(p) , mod = require.modules[path]; if (!mod) throw new Error('failed to require "' + p + '"'); if (!mod.exports) { mod.exports = {}; mod.call(mod.exports, mod, mod.exports, require.relative(path)); } return mod.exports;}require.modules = {};require.resolve = function(path){ var orig = path , reg = path + '.js' , index = path + '/index.js'; return require.modules[reg] && reg || require.modules[index] && index || orig;};require.register = function(path, fn){ require.modules[path] = fn;};require.relative = function(parent) { return function(p){ if ('.' != p.charAt(0)) return require(p); var path = parent.split('/') , segs = p.split('/'); path.pop(); for (var i = 0; i < segs.length; i++) { var seg = segs[i]; if ('..' == seg) path.pop(); else if ('.' != seg) path.push(seg); } return require(path.join('/')); };};require.register("plurals.js", function(module, exports, require){
+'use strict';
+
+/*
+ * shamelessly taken from https://github.com/masylum/dialect/blob/master/lib/helpers/plurals.js.
+ * Thank you, @masylum
+ */
+
+/* plurals.js provide functions that give you the plural index
+ * for any locale.
+ *
+ * Usage:
+ *  require('plurals')('es')(3) => 1;
+ *  require('plurals')('es')(1) => 0;
+ *
+ * please, add your language if its not represented.
+ *
+ * references:
+ *
+ * http://unicode.org/repos/cldr-tmp/trunk/diff/supplemental/language_plural_rules.html
+ * http://translate.sourceforge.net/wiki/l10n/pluralforms
+ * http://www.gnu.org/software/gettext/manual/gettext.html#Plural-forms
+ *
+ */
+
+module.exports = function plurals(locale) {
+  var parts = locale.split('-');
+
+  switch (locale) {
+
+  // 1 Plural
+  case 'ja':
+  case 'vi':
+  case 'ko':
+    return function (n) {
+      return 1;
+    };
+
+  // 2 Plurals
+  case 'pt-BR':
+  case 'fr':
+    return function (n) {
+      return n > 1 ? 1 : 0;
+    };
+
+  // 3 Plurals
+  case 'lv':
+    return function (n) {
+      return n % 10 === 1 && n % 100 !== 11 ? 0 : n !== 0 ? 1 : 2;
+    };
+  case 'br':
+  case 'ga':
+  case 'gd':
+  case 'cy':
+    return function (n) {
+      return n === 1 ? 0 : n === 2 ? 1 : 2;
+    };
+  case 'ro':
+    return function (n) {
+      return n === 1 ? 0 : (n === 0 || (n % 100 > 0 && n % 100 < 20)) ? 1 : 2;
+    };
+  case 'lt':
+    return function (n) {
+      return n % 10 === 1 && n % 100 !== 11 ? 0 : n % 10 >= 2 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2;
+    };
+  case 'ru':
+  case 'uk':
+  case 'sr':
+  case 'hr':
+  case 'sh':
+    return function (n) {
+      return n % 10 === 1 && n % 100 !== 11 ? 0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2;
+    };
+  case 'cs':
+  case 'sk':
+    return function (n) {
+      return (n === 1) ? 0 : (n >= 2 && n <= 4) ? 1 : 2;
+    };
+  case 'pl':
+    return function (n) {
+      return n === 1 ? 0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2;
+    };
+  case 'mk':
+    return function (n) {
+      return n === 1 ? 0 : n % 10 >= 2 ? 1 : 2;
+    };
+  case 'sl':
+    return function (n) {
+      return n % 100 === 1 ? 0 : n % 100 === 2 ? 1 : n % 100 === 3 || n % 100 === 4 ? 2 : 3;
+    };
+
+  // 2 Plurals
+  default:
+    if (parts.length === 2) {
+      plurals(parts[0]);
+    } else {
+      return function (n) {
+        return n === 1 ? 0 : 1;
+      };
+    }
+  }
+};
+
+});require.register("index.js", function(module, exports, require){
+'use strict';
+
+var C = require('./compose');
+var plurals = require('./plurals');
+
+//
+// curry function
+//
+var slice = Array.prototype.slice;
+var nativeBind = Function.prototype.bind;
+function bind(func, obj) {
+  if (func.bind === nativeBind && nativeBind) {
+    return nativeBind.apply(func, slice.call(arguments, 1));
+  }
+  var args = slice.call(arguments, 2);
+  return function() {
+    return func.apply(obj, args.concat(slice.call(arguments)));
+  };
+}
+
+//
+// Locale
+//
+
+function Locale() {
+}
+
+//
+// get pluralize function
+//
+// FIXME: should bring rules in here
+//
+Locale.prototype.p = function (id) {
+  // TODO: cache?
+  var fn = plurals(id);
+  return function(n, forms) {
+    // TODO: consider guessing for languages with simple plural
+    return forms[fn(n)] || forms[1] || forms[0] || '???';
+  };
+};
+
+//
+// add a `hash` of strings to the locale. `id` defines language
+//
+Locale.prototype.add = function (id, hash) {
+
+  var self = this;
+
+  //
+  // JavaScript micro-templating, similar to John Resig's implementation
+  //
+  var re_interpolate1 = /#\{([\s\S]+?)\}/g;
+  var re_interpolate2 = /%\{([\s\S]+?)\}:([\w.$]+)/g;
+
+  var re_inflect = /^([\w.$]+?) +([\s\S]+)/;
+
+  function expand(str) {
+    str = String(str || '');
+    var interpolated = false;
+    var nonce = '~~~' + Math.random().toString().substring(2) + '~~~';
+    var re_unescape = new RegExp(nonce, 'g');
+    var tmpl = "var __p=this.p('" + id + "');with(locals||{}){return ['" + str
+      .replace(/\\/g, '\\\\')
+      .replace(/'/g, "\\'")
+      // }} are escaped to not interfere with closing }
+      // FIXME: this is redundant for simple strings!
+      .replace(/\}\}/g, nonce)
+      // #{var forms...}
+      .replace(re_interpolate1, function (match, inner) {
+        interpolated = true;
+        inner = inner.replace(/\\'/, "'");
+        var m = re_inflect.exec(inner);
+        if (m) {
+          // unescape }} and parse word forms
+          var forms = m[2].replace(re_unescape, '}').split('|');
+          return '\',__p(' + m[1] + ',' + JSON.stringify(forms) + '),\'';
+        // #{this.foo.bar} is sugar for this.t('foo.bar', locals)
+        } else if (inner.indexOf('this.') === 0) {
+          return '\',this.t("' + inner.substring(5) + '",locals),\'';
+        } else {
+          return '\',' + inner + ',\'';
+        }
+      })
+      // %{forms...}:var
+      .replace(re_interpolate2, function (match, forms, variable) {
+        interpolated = true;
+        forms = forms.replace(/\\'/, "'");
+        variable = variable.replace(/\\'/, "'");
+        // unescape }} and parse word forms
+        forms = forms.replace(re_unescape, '}').split('|');
+        return '\',__p(' + variable + ',' + JSON.stringify(forms) + '),\'';
+      })
+      // unescape orphan }}
+      .replace(re_unescape, '}')
+      + "'].join('')}";
+    //return interpolated ? tmpl : str;
+    //console.log(tmpl);
+    if (interpolated) {
+      var fn = bind((new Function('locals', tmpl)), self);
+      fn.body = tmpl;
+      return fn;
+    } else {
+      return str;
+    }
+  }
+
+  function clone(o, c, path) {
+    var i, p;
+    for (i in o) {
+      if (o.hasOwnProperty(i)) {
+        p = path ? path + '.' + i : i;
+        if (o[i] && typeof o[i] === 'object') {
+          c[i] = {};
+          clone(o[i], c[i], p);
+        } else if (typeof o[i] === 'string') {
+          c[i] = expand(o[i], id);
+        } else {
+          c[i] = o[i];
+        }
+        //c[p] = c[i];
+      }
+    }
+  }
+  var copy = {};
+  clone(hash, copy);
+
+  // augment current locale hash
+  C.call(this, copy);
+
+  return this;
+};
+
+//
+// get a subtree of locale.
+// honors dotted notation: `get('foo.bar.baz') === get('foo').bar.baz`
+//
+Locale.prototype.get = function (key) {
+  var path = (key || '').split('.');
+  var o = this;
+  var p;
+  while (o && (p = path.shift())) {
+    o = o[p];
+  }
+  return o;
+};
+
+//
+// evaluator helper
+//
+Locale.prototype.t = function (key, vars) {
+  var fn = this.get(key);
+  if (typeof fn === 'function') {
+    return fn.call(this, vars);
+  } else {
+    return fn;
+  }
+};
+
+//
+// export subtree as string
+//
+// FIXME: this is crap, really. Find out another ways to deliver locales over the wire
+//
+Locale.prototype.export = function (key) {
+  var scope = this.get(key);
+  // FIXME: ensure JSON exists it browser
+  return JSON.stringify(scope, function (key, value) {
+    if (typeof value === 'function') {
+      // N.B. we loose `this` here
+      value = 'f' + value.body;
+    } else if (typeof value === 'string') {
+      value = 's' + value;
+    } else {
+      value = undefined;
+    }
+    return value;
+  });
+};
+
+//
+// module
+//
+module.exports = Locale;
+
+});require.register("compose.js", function(module, exports, require){
 /*
 * ComposeJS, object composition for JavaScript, featuring
 * JavaScript-style prototype inheritance and composition, multiple inheritance,
@@ -304,311 +592,6 @@ function getConstructors(args) {
 
 // export
 module.exports = Compose;
-
-});require.register("plurals.js", function(module, exports, require){
-'use strict';
-
-/*
- * shamelessly taken from https://github.com/masylum/dialect/blob/master/lib/helpers/plurals.js.
- * Thank you, @masylum
- */
-
-/* plurals.js provide functions that give you the plural index
- * for any locale.
- *
- * Usage:
- *  require('plurals')('es')(3) => 1;
- *  require('plurals')('es')(1) => 0;
- *
- * please, add your language if its not represented.
- *
- * references:
- *
- * http://unicode.org/repos/cldr-tmp/trunk/diff/supplemental/language_plural_rules.html
- * http://translate.sourceforge.net/wiki/l10n/pluralforms
- * http://www.gnu.org/software/gettext/manual/gettext.html#Plural-forms
- *
- */
-
-module.exports = function plurals(locale) {
-  var parts = locale.split('-');
-
-  switch (locale) {
-
-  // 1 Plural
-  case 'ja':
-  case 'vi':
-  case 'ko':
-    return function (n) {
-      return 1;
-    };
-
-  // 2 Plurals
-  case 'pt-BR':
-  case 'fr':
-    return function (n) {
-      return n > 1 ? 1 : 0;
-    };
-
-  // 3 Plurals
-  case 'lv':
-    return function (n) {
-      return n % 10 === 1 && n % 100 !== 11 ? 0 : n !== 0 ? 1 : 2;
-    };
-  case 'br':
-  case 'ga':
-  case 'gd':
-  case 'cy':
-    return function (n) {
-      return n === 1 ? 0 : n === 2 ? 1 : 2;
-    };
-  case 'ro':
-    return function (n) {
-      return n === 1 ? 0 : (n === 0 || (n % 100 > 0 && n % 100 < 20)) ? 1 : 2;
-    };
-  case 'lt':
-    return function (n) {
-      return n % 10 === 1 && n % 100 !== 11 ? 0 : n % 10 >= 2 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2;
-    };
-  case 'ru':
-  case 'uk':
-  case 'sr':
-  case 'hr':
-  case 'sh':
-    return function (n) {
-      return n % 10 === 1 && n % 100 !== 11 ? 0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2;
-    };
-  case 'cs':
-  case 'sk':
-    return function (n) {
-      return (n === 1) ? 0 : (n >= 2 && n <= 4) ? 1 : 2;
-    };
-  case 'pl':
-    return function (n) {
-      return n === 1 ? 0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2;
-    };
-  case 'mk':
-    return function (n) {
-      return n === 1 ? 0 : n % 10 >= 2 ? 1 : 2;
-    };
-  case 'sl':
-    return function (n) {
-      return n % 100 === 1 ? 0 : n % 100 === 2 ? 1 : n % 100 === 3 || n % 100 === 4 ? 2 : 3;
-    };
-
-  // 2 Plurals
-  default:
-    if (parts.length === 2) {
-      plurals(parts[0]);
-    } else {
-      return function (n) {
-        return n === 1 ? 0 : 1;
-      };
-    }
-  }
-};
-
-});require.register("index.js", function(module, exports, require){
-'use strict';
-
-var C = require('./compose');
-var plurals = require('./plurals');
-
-//
-// curry function
-//
-var slice = Array.prototype.slice;
-var nativeBind = Function.prototype.bind;
-function bind(func, obj) {
-  if (func.bind === nativeBind && nativeBind) {
-    return nativeBind.apply(func, slice.call(arguments, 1));
-  }
-  var args = slice.call(arguments, 2);
-  return function() {
-    return func.apply(obj, args.concat(slice.call(arguments)));
-  };
-}
-
-//
-// Locale
-//
-
-function Locale() {
-}
-
-//
-// get pluralize function
-//
-// FIXME: should bring rules in here
-//
-Locale.prototype.p = function (id) {
-  // TODO: cache?
-  var fn = plurals(id);
-  return function(n, forms) {
-    // TODO: consider guessing for languages with simple plural
-    return forms[fn(n)] || forms[1] || forms[0] || '???';
-  };
-};
-
-//
-// add a `hash` of strings to the locale. `id` defines language
-//
-Locale.prototype.add = function (id, hash) {
-
-  var self = this;
-
-  //
-  // JavaScript micro-templating, similar to John Resig's implementation
-  //
-  var re_interpolate1 = /#\{([\s\S]+?)\}/g;
-  var re_interpolate2 = /%\{([\s\S]+?)\}:([\w.$]+)/g;
-
-  var re_inflect = /^([\w.$]+?) +([\s\S]+)/;
-
-  function expand(str) {
-    str = String(str || '');
-    var interpolated = false;
-    var nonce = '~~~' + Math.random().toString().substring(2) + '~~~';
-    var re_unescape = new RegExp(nonce, 'g');
-    var tmpl = "var __p=this.p('" + id + "');with(locals||{}){return ['" + str
-      .replace(/\\/g, '\\\\')
-      .replace(/'/g, "\\'")
-      // }} are escaped to not interfere with closing }
-      // FIXME: this is redundant for simple strings!
-      .replace(/\}\}/g, nonce)
-      // #{var forms...}
-      .replace(re_interpolate1, function (match, inner) {
-        interpolated = true;
-        inner = inner.replace(/\\'/, "'");
-        var m = re_inflect.exec(inner);
-        if (m) {
-          // unescape }} and parse word forms
-          var forms = m[2].replace(re_unescape, '}').split('|');
-          return '\',__p(' + m[1] + ',' + JSON.stringify(forms) + '),\'';
-        // #{this.foo.bar} is sugar for this.t('foo.bar', locals)
-        } else if (inner.indexOf('this.') === 0) {
-          return '\',this.t("' + inner.substring(5) + '",locals),\'';
-        } else {
-          return '\',' + inner + ',\'';
-        }
-      })
-      // %{forms...}:var
-      .replace(re_interpolate2, function (match, forms, variable) {
-        interpolated = true;
-        forms = forms.replace(/\\'/, "'");
-        variable = variable.replace(/\\'/, "'");
-        // unescape }} and parse word forms
-        forms = forms.replace(re_unescape, '}').split('|');
-        return '\',__p(' + variable + ',' + JSON.stringify(forms) + '),\'';
-      })
-      // unescape orphan }}
-      .replace(re_unescape, '}')
-      + "'].join('')}";
-    //return interpolated ? tmpl : str;
-    //console.log(tmpl);
-    if (interpolated) {
-      var fn = bind((new Function('locals', tmpl)), self);
-      fn.body = tmpl;
-      return fn;
-    } else {
-      return str;
-    }
-  }
-
-  function clone(o, c, path) {
-    var i, p;
-    for (i in o) {
-      if (o.hasOwnProperty(i)) {
-        p = path ? path + '.' + i : i;
-        if (o[i] && typeof o[i] === 'object') {
-          c[i] = {};
-          clone(o[i], c[i], p);
-        } else if (typeof o[i] === 'string') {
-          c[i] = expand(o[i], id);
-        } else {
-          c[i] = o[i];
-        }
-        //c[p] = c[i];
-      }
-    }
-  }
-  var copy = {};
-  clone(hash, copy);
-
-  // augment current locale hash
-  C.call(this, copy);
-
-  return this;
-};
-
-//
-// get a subtree of locale.
-// honors dotted notation: `get('foo.bar.baz') === get('foo').bar.baz`
-//
-Locale.prototype.get = function (key) {
-  var path = (key || '').split('.');
-  var o = this;
-  var p;
-  while (o && (p = path.shift())) {
-    o = o[p];
-  }
-  return o;
-};
-
-//
-// evaluator helper
-//
-Locale.prototype.t = function (key, vars) {
-  var fn = this.get(key);
-  if (typeof fn === 'function') {
-    return fn.call(this, vars);
-  } else {
-    return fn;
-  }
-};
-
-//
-// export subtree as string
-//
-// FIXME: this is crap, really. Find out another ways to deliver locales over the wire
-//
-Locale.prototype.export = function (key) {
-  var scope = this.get(key);
-  // FIXME: ensure JSON exists it browser
-  return JSON.stringify(scope, function (key, value) {
-    if (typeof value === 'function') {
-      // N.B. we loose `this` here
-      value = 'f' + value.body;
-    } else if (typeof value === 'string') {
-      value = 's' + value;
-    } else {
-      value = undefined;
-    }
-    return value;
-  });
-};
-
-//
-// module
-//
-module.exports = Locale;
-
-var L = new Locale();
-L.add('en', {foo:'You\'ve got #{wives} #{wives wife|wives} and #{children} %{child|childre n\'|kindern}:children'});
-L.add('ru', {bar:'У тебя #{wives} #{wives   жена|жены|жён} и #{children} %{ребёнок}}|ребёнка}}}}|детей}}}}}}}:children'});
-L.add('fr', {baz:'C\'est une texte simple'});
-L.add('fr', {bu1:'C\'est une texte simple. Plus, reused #{this.bar}'});
-L.add('fr', {bu2:'C\'est une texte simple. Plus, reused #{this.baz}'});
-console.log(L.get('foo').toString());
-console.log(L.get('bar').toString());
-//console.log((new Function('obj', L.get('foo'))).call(L, {wives: 2, children: 5}));
-console.log(L.t('bar', {wives: 2, children: 5}));
-console.log(L.get('baz'));
-console.log(L.get('foo')({wives: 1, children: 1}));
-console.log(L.bar({wives: 5, children: 101}));
-console.log(L.bu1({wives: 1, children: 11}));
-console.log(L.bu2({wives: 1, children: 11}));
-console.log(L.export(''));
 
 });Locale = require('index.js');
 })();
